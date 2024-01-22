3bb1158d927e5a41e765ac4e6c2e1eba057570c2
andyjaramillo <andy.jaramillo022@gmail.com>
2024-01-21 21:37:57 -0500

    weird error

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,7 +1,7 @@
 
 import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 
 const DefaultLayout = () => {
 
