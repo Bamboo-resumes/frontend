7800031c25400b4828243e1aeec1b942a5a98298
andyjaramillo <andy.jaramillo022@gmail.com>
2024-01-23 08:55:51 -0500

    changed to white text

--- src/components/FrontFace.tsx
+++ src/components/FrontFace.tsx
@@ -14,8 +14,8 @@ function FrontFace(props: FrontFaceProps) {
     <div class="px-4 mx-auto max-w-screen-l  text-center py-24 lg:py-56  " style={{backgroundImage: `url('${"frontfacebg.png"}')`,
 backgroundPosition: 'center',
 backgroundSize: 'cover',}}>
-        <h1  class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-black md:text-5xl lg:text-6xl">Master your job seeking journey</h1>
-        <p class="mb-8 text-lg font-normal text-black lg:text-xl sm:px-16 lg:px-48">Here at Bamboo we tailor your resumes to the job description, saving you time on each application</p>
+        <h1  class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Master your job seeking journey</h1>
+        <p class="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 lg:px-48">Here at Bamboo we tailor your resumes to the job description, saving you time on each application</p>
         <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <button onClick={scrollTo} class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Get started
